32eb0288676f6759b51208466e16392de8fdc17d
illya15 <113913094+illya15@users.noreply.github.com>
2023-05-06 23:48:25 +0300

    task1 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import React from 'react';
+
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions'
+
+
+ class App extends Component {
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+
+
+
+
+render() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    
+  <>
+    <Section     title ="Please leav feedback">
+ <FeedbackOptions options={['good','neitral', 'bed']}/> 
+    
+    
+   </Section>
+
+   </>
+  )
+}
+ }
+
+ export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,8 @@
+import { FeedbackOptions } from './FeedbackOptions';
+const Section = ({title,FeedbackOptions}) => {
+
+<button type="button" key={item} onClick={onLeavFeedback}>
+  {item}
+</button>; 
+}
+export default Section
