64038dc8ce6a36b4696304c61282eb1ef265f7a8
illya15 <113913094+illya15@users.noreply.github.com>
2023-05-10 13:25:10 +0300

    task17

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -38,27 +38,39 @@ class App extends Component {
   render() {
     return (
       <>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleCounter}
-          ></FeedbackOptions>
-        </Section>
-        <Section title={'Statistics'}>
-          {this.countTotalFeedback(this.state) > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback(this.state)}
-              positivePercentage={this.countPositiveFeedbackPercentage(
-                this.state
-              )}
-            ></Statistics>
-          ) : (
-            <Notification message={'There is feedback'} />
-          )}
-        </Section>
+        <div
+          style={{
+            // height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          <Section title={'Please leave feedback'}>
+            <FeedbackOptions
+              options={['good', 'neutral', 'bad']}
+              onLeaveFeedback={this.handleCounter}
+            ></FeedbackOptions>
+          </Section>
+          <Section title={'Statistics'}>
+            {this.countTotalFeedback(this.state) > 0 ? (
+              <Statistics
+                good={this.state.good}
+                neutral={this.state.neutral}
+                bad={this.state.bad}
+                total={this.countTotalFeedback(this.state)}
+                positivePercentage={this.countPositiveFeedbackPercentage(
+                  this.state
+                )}
+              ></Statistics>
+            ) : (
+              <Notification message={'There is feedback'} />
+            )}
+          </Section>
+        </div>
       </>
     );
   }
